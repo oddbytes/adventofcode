16e06e26453a4b771bf6c53f5f41bee10756b2dc
oddbytes <mack@oddbytes.net>
2020-12-01 13:51:57 +0100

    2020 - day 1 - part 1- reduced complexity to On

--- src/2020/Day 1/part1.ts
+++ src/2020/Day 1/part1.ts
@@ -5,6 +5,7 @@ class ExpenseReport {
 
   public findCosts = (): number[] => {
     console.time("findCost");
+
     // Ordenamos los numeros
     this.expenses = this.expenses.sort((a, b) => a - b);
 
@@ -24,8 +25,29 @@ class ExpenseReport {
 
     return costs;
   };
+
+  public findCostsN = (): number[] => {
+    console.time("findCostN");
+
+    // Ordenamos los numeros
+    this.expenses = this.expenses.sort();
+    let start = 0,
+      end = this.expenses.length - 1;
+    // Ajustar los indices teniendo en cuenta la ordenacion
+
+    while (this.expenses[start] + this.expenses[end] !== 2020)
+      if (this.expenses[start] + this.expenses[end] > 2020) end--;
+      else start++;
+
+    console.timeEnd("findCostN");
+    return [this.expenses[start], this.expenses[end]];
+  };
 }
 
 const costs = new ExpenseReport(expenses).findCosts();
 console.log(costs);
 console.log("Answer:", costs[0] * costs[1]);
+
+const costs2 = new ExpenseReport(expenses).findCostsN();
+console.log(costs2);
+console.log("Answer:", costs2[0] * costs2[1]);
