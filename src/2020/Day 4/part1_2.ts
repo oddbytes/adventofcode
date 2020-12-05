53c3fc5698e4d51e8cbbc924ad313bd4418ba41d
oddbytes <mack@oddbytes.net>
2020-12-05 07:55:21 +0100

    Type annotation

--- src/2020/Day 4/part1_2.ts
+++ src/2020/Day 4/part1_2.ts
@@ -77,7 +77,7 @@ class Passport {
 }
 
 class PassportDatabase {
-  public read = (file: string) =>
+  public read = (file: string): Passport[] =>
     fs
       .readFileSync(file, "utf8")
       .split("\r\n\r\n")
