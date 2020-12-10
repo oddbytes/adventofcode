4799db2f7f4111531b3882d17d094fb08deef063
oddbytes <mack@oddbytes.net>
2020-12-10 14:27:29 +0100

    2015 day 4 part 1

---
+++ src/2015/Day 4/part1.ts
@@ -0,0 +1,9 @@
+import { Md5 } from "ts-md5"; //make sure it points to the folder where the md5.ts file is
+
+const start = "iwrupvqb";
+let i = 0;
+let hash = "";
+while (!hash.startsWith("00000")) {
+  hash = Md5.hashStr(start + (i++).toString());
+}
+console.log(i - 1, hash);
