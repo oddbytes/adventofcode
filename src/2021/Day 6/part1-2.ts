60747d2b8588f1b06849368f442dc66e705d9773
oddbytes <mack@oddbytes.net>
2021-12-06 10:47:42 +0100

    2021 day 6

---
+++ src/2021/Day 6/part1-2.ts
@@ -0,0 +1,33 @@
+import * as fs from "fs";
+
+console.time("totalTime");
+
+// array cotaining number of fishes per age (index)
+const ages = new Array(9).fill(0);
+
+fs.readFileSync("./puzzle.txt", "utf8")
+  .split(",")
+  .forEach((elem) => ages[elem]++);
+
+const cycle = (ages: number[]): void => {
+  const newBorns = ages[0]; //fishes to born in this cycle
+  ages.shift();
+  ages[8] = newBorns;
+  ages[6] += newBorns; //timer resets
+};
+
+for (let day = 0; day < 80; day++) cycle(ages);
+
+console.log(
+  "Response part 1:",
+  ages.reduce((a, b) => (a += b)) //sum of fishes of all ages
+);
+
+for (let day = 0; day < 256 - 80; day++) cycle(ages);
+
+console.log(
+  "Response part 2:",
+  ages.reduce((a, b) => (a += b)) //sum of fishes of all ages
+);
+
+console.timeEnd("totalTime");
