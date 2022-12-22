4d8e3906ea45c0bd1483b114731c5c2fe6386b70
Jesus Ma Delgado Anton <jesus.delgado.anton@accenture.com>
2022-12-22 18:13:54 +0100

    2022 Day 5 part 1

--- src/2022/5/part1&2.ts
+++ src/2022/5/part1&2.ts
@@ -1,17 +1,45 @@
 import * as fs from "fs";
 
 console.time("day4");
-let [stacks, instructions] = fs
-  .readFileSync("./sample.txt", "utf8")
-  .split("\r\n\r\n");
+const [stacksConfig, instructions] = fs
+  .readFileSync("./input.txt", "utf8")
+  .split("\r\n\r\n")
+  .map((p) => p.split("\r\n"));
 
-  stacks=stacks.split("\r\n").map(l=>)
+const stackNumbers = stacksConfig[stacksConfig.length - 1]
+  .trimEnd()
+  .split(" ")
+  .map(Number);
 
-console.log(stacks, instructions);
+const numberOfStacks = stackNumbers[stackNumbers.length - 1];
+
+const stacks: string[][] = new Array(numberOfStacks - 1);
+for (let i = 0; i < numberOfStacks; i++) stacks[i] = [];
+
+console.log(numberOfStacks, stacks, instructions);
+
+for (let i = 0; i < stacksConfig.length - 1; i++)
+  for (let s = 0; s < numberOfStacks; s++)
+    if (stacksConfig[i][1 + s * 4] != " ")
+      stacks[s].push(stacksConfig[i][1 + s * 4]);
+
+for (let i = 0; i < numberOfStacks; i++) stacks[i] = stacks[i].reverse();
+
+const re = /move (\d+) from (\d+) to (\d+)/;
+instructions.forEach((instruction) => {
+  const [_match, quantity, from, to] = re.exec(instruction);
+
+  for (let step = 0; step < parseInt(quantity); step++) {
+    const crate = stacks[parseInt(from) - 1].pop();
+    stacks[parseInt(to) - 1].push(crate);
+  }
+});
+
+console.log(numberOfStacks, stacks);
 
 console.time("part1");
 
-console.log(`Part1 response: `);
+console.log(`Part1 response: ${stacks.map((s) => s[s.length - 1]).join("")}`);
 
 console.timeEnd("part1");
 
