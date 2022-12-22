b01b8d62db515b2d36139d995570f51954f0fe6c
oddbytes <mack@oddbytes.net>
2022-12-22 21:39:24 +0100

    2022 d6 p2

--- src/2022/6/part1&2.ts
+++ src/2022/6/part1&2.ts
@@ -3,16 +3,16 @@ import * as fs from "fs";
 console.time("day6");
 const stream = fs.readFileSync("./input.txt", "utf8");
 
-const part1 = (stream: string) => {
-  console.log(stream);
-  let position = 3;
+const part1 = (stream: string, packetSize: number) => {
+  //console.log(stream);
+  let position = packetSize - 1;
   //advance till last four chars are all dferent
   let markerFound = false;
   while (!markerFound && position < stream.length) {
     position++;
-    console.log(position, stream.slice(position - 4, position));
+
     markerFound = stream
-      .slice(position - 4, position)
+      .slice(position - packetSize, position)
       .split("")
       .every((c, i, segment) => segment.lastIndexOf(c) == i);
   }
@@ -21,13 +21,13 @@ const part1 = (stream: string) => {
 
 console.time("part1");
 
-console.log(`Part1 response: ${part1(stream)}`);
+console.log(`Part1 response: ${part1(stream, 4)}`);
 
 console.timeEnd("part1");
 
 console.time("part2");
 
-console.log(`Part2 response: `);
+console.log(`Part2 response: ${part1(stream, 14)}`);
 
 console.timeEnd("part2");
 
