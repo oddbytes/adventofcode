62740d26a1a7e14db0c3876ae4f7d7ddf2034b42
mack <mack@oddbytes.net>
2025-12-05 18:23:17 +0100

    day 3

--- src/2025/Day 3/parts1and2.ts
+++ src/2025/Day 3/parts1and2.ts
@@ -19,7 +19,23 @@ console.timeEnd("Tiempo de ejecucion parte 1");
 console.log("Solucion parte 1: " + solucion);
 //Parte 2: hay que contar las veces que el dial pasa por 0, no solo las que es exactamente 0.
 console.time("Tiempo de ejecucion parte 2");
+//En la parte 2 necesitamos encontrar la combinacion de 12 digitos mayores posible
+solucion = 0;
+//Para cada banco hallar el numero mayor desde la posicion actual hasta la lenght-paso-1
 
+banks.forEach((bank) => {
+  let joltage = 0;
+  let currPos = 0;
+  for (let paso = 1; paso <= 12; paso++) {
+    const maxInRange = Math.max(
+      ...bank.slice(currPos, bank.length - (12 - paso))
+    );
+    currPos = bank.indexOf(maxInRange, currPos) + 1;
+    joltage += maxInRange * 10 ** (12 - paso);
+  }
+  // console.log(`Joltage final para el banco: ${joltage}`);
+  solucion += joltage;
+});
 console.timeEnd("Tiempo de ejecucion parte 2");
 console.log("Solucion parte 2: " + solucion);
 
